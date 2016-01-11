3447255b88f3bcb3a69a4270f51518ebe36e49e8
jakelin212 <jlin212@gmail.com>
2016-01-11 13:42:21 +0200

    css structure

--- info.js
+++
@@ -1,7 +0,0 @@
-function displayInfo(){
-	//$("#HInfo").html("Contact/Source");
-}
-
-function helpHemap(){
-	new Messi('Display link to user guide and about submitted manuscript', {title: 'Info'});	
-}
